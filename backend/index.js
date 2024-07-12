c59215687fcd2e9c8813750040bd63770f19e007
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-12 23:57:39 +0530

    adding backend comtent

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
+
 
 dotenv.config();
 
@@ -13,17 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-// Simple test route
-app.get('/', (req, res) => {
-    res.send('Server is running');
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.use("/api/user", userRoute)
