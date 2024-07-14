5f487e700c5ecf2c63d608a933e514d830b99e2e
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-14 13:38:30 +0530

    adding residency

--- backend/controllers/userCntrl.js
+++ backend/controllers/userCntrl.js
@@ -6,5 +6,16 @@ export const createUser = asyncHandler(async (req, res) => {
 
     let { email } = req.body
 
-    console.log(email)
+    // console.log(email)
+    const userExists = await prisma.user.findUnique({ where: { email: email } })
+    if (!userExists) {
+        const user = await prisma.user.create({ data: req.body });
+        res.send({
+            message: "User registered successfully",
+            user: user,
+        })
+    }
+    else {
+        res.status(201).send({ message: "User already registered" })
+    }
 })
