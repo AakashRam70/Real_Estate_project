1350431c48bc16d983ee4f83e767bfc684a4a6f4
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-14 21:14:13 +0530

    visits book

--- backend/controllers/userCntrl.js
+++ backend/controllers/userCntrl.js
@@ -19,3 +19,30 @@ export const createUser = asyncHandler(async (req, res) => {
         res.status(201).send({ message: "User already registered" })
     }
 })
+
+// to book a visit to resd 
+export const bookVisit = asyncHandler(async (req, res) => {
+    const { email, data } = req.body;
+    const { id } = req.params
+
+    try {
+        const alreadyBooked = await prisma.user.findUnique({
+            where: { email },
+            select: { bookedVisits: true }
+        })
+
+        if (alreadyBooked.bookedVisits.some((visit) => visit.id === id)) {
+            res.status(400).send({ message: "This residency already booked by you" })
+        } else {
+            await prisma.user.update({
+                where: { email: email },
+                data: {
+                    bookedVisits: { push: { id: id } },
+                },
+            })
+        }
+        res.send("Your visit is booked successfully..")
+    } catch (error) {
+        throw new Error(err.message)
+    }
+})
