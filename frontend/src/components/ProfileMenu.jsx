ec83634aa0311fe64734c216050c960dea6a8b12
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-16 09:52:52 +0530

    adding new

--- frontend/src/components/ProfileMenu.jsx
+++ frontend/src/components/ProfileMenu.jsx
@@ -8,15 +8,18 @@ const ProfileMenu = () => {
                 <Avatar src={user?.picture} alt="userImage" radius={"xl"} />
             </Menu.Target>
             <Menu.Dropdown>
-                <Menu.Item>
-                    Favourites
-                </Menu.Item>
-                <Menu.Item>
-                    Bookings
-                </Menu.Item>
+                <Menu.Item>Favourites</Menu.Item>
+                <Menu.Item>Bookings</Menu.Item>
+                <Menu.Item>Application</Menu.Item>
+                <Menu.Item>Go back</Menu.Item>
+                <Menu.Item onClick={() => {
+                    localStorage.clear();
+                    logOut();
+                }}
+                    color="red">Logout</Menu.Item>
             </Menu.Dropdown>
         </Menu>
     )
 }
 
-export default ProfileMenu
+export default ProfileMenu;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Auth0Provider } from "@auth0/auth0-react"
+import { Auth0Provider } from "@auth0/auth0-react";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/dates/styles.css"
+import "@mantine/core/styles.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
-    domain='dev-lfg5kzol7aqrnlu6.us.auth0.com'
-    clientId='oos8rA5ckiVZJRI65tRYJEeefKtCQXZZ'
-    authorizationParams={{
-      redirect_uri:"http://localhost:5173"
-    }}
-    audience="http://localhost:8000"
-    scope="openid profile email"
+      domain='dev-lfg5kzol7aqrnlu6.us.auth0.com'
+      clientId='oos8rA5ckiVZJRI65tRYJEeefKtCQXZZ'
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173"
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
     >
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </Auth0Provider>
   </React.StrictMode>,
 )
