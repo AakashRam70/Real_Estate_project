c340629e698668455a7be90b6c8cda8403a8c3f0
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-17 23:58:54 +0530

    adding content

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { allBookings, bookVisit, cancleBooking, createUser, getAllFav, toFav } from "../controllers/userCntrl.js";
+import jwtCheck from "../config/auth0config.js";
 const router = express.Router()
 
-router.post("/register", createUser)
+router.post("/register", jwtCheck, createUser)
 router.post("/bookVisit/:id", bookVisit)
 router.post("/allBookings", allBookings)
 router.post("/removeBooking/:id", cancleBooking)
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -11,12 +11,27 @@ const Layout = () => {
     const { isAuthenticated, user, getAccessTokenWithPopup } = useAuth0();
     const { setUserDetails } = useContext(UserDetailContext)
     const { mutate } = useMutation({
-        mutationKey: [user?.email],
+        mutationKey: [user?.email, token],
         mutationFn: () => createUser(user?.email)
     });
 
     useEffect(() => {
-        isAuthenticated && mutate()
+
+        const getTokenAndRegister = async () => {
+            const res = await getAccessTokenWithPopup({
+                authorizationParams: {
+                    audience: "http://localhost:8000",
+                    scope: "openid profile email"
+                }
+            })
+            localStorage.setItem("access_token", res)
+            setUserDetails((prev) => ({ ...prev, token: res }));
+
+            mutate(res)
+        }
+
+
+        isAuthenticated && getTokenAndRegister()
     }, [isAuthenticated])
 
     return (
