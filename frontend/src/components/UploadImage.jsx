a1ae425ba4c94c0aa097f6ee011f09fc1afd11b1
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-24 17:04:54 +0530

    adding step 2 verification and image url

--- frontend/src/components/UploadImage.jsx
+++ frontend/src/components/UploadImage.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import { Button, Group } from '@mantine/core';
+import React, { useEffect, useRef, useState } from 'react'
+import { MdOutlineCloudUpload } from 'react-icons/md'
 
 const UploadImage = ({
     propertyDetails,
@@ -6,20 +8,48 @@ const UploadImage = ({
     prevStep,
     nextStep }) => {
 
-    const [imageURL, setImageURL] = useState(propertyDetails.image)
+    const [imageURL, setImageURL] = useState(propertyDetails.image);
+    const cloudinaryRef = useRef();
+    const widgetRef = useRef();
+
+    useEffect(() => {
+        cloudinaryRef.current = window.cloudinary;
+        widgetRef.current = cloudinaryRef.current.createUploadWidget(
+            {
+                cloudName: "dxcbugaoq",
+                uploadPreset: "npkuj8ar",
+                maxFile: 1
+            },
+            (err, result) => {
+                if (result.event === "success") {
+                    setImageURL(result.info.secure_url)
+                }
+            }
+        )
+    }, [])
 
     return (
-        <div>
+        <div className='flexCenter mt-9 flex-col'>
             {
                 !imageURL ? (
-                    <div className='flexCenter flex-col w-3/4 h-[21rem] border-dashed border-2 cursor-pointer'>
-                        <MdOutlineCloudUpload size={44} color='gray'/>
-
-
-                ): ()
-            }
+                    <div
+                        onClick={() => widgetRef.current?.open()}
+                        className='flexCenter flex-col w-3/4 h-[21rem] border-dashed border-2 cursor-pointer'>
+                        <MdOutlineCloudUpload size={50} color='gray' />
+                        <span>Upload Images</span>
+                    </div>
+                ) : (
+                    <div
+                        onClick={() => widgetRef.current?.open()} className='w-3/4 h-[22rem] rounded-xl cursor-pointer overflow-hidden'>
+                        <img src={imageURL} alt="imageURL" className='h-full w-full object-cover' />
+                    </div>
+                )}
+            <Group justify="center" mt="xl">
+                <Button onClick={prevStep}>Prev step</Button>
+                <Button onClick={nextStep}>Next step</Button>
+            </Group>
         </div>
     )
 }
 
-export default UploadImage
+export default UploadImage;
