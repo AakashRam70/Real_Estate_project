93dfded8fc268606fbe2ab821929d33a682e6b5a
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-07-25 14:41:09 +0530

    improving search button

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { FaLocationDot } from 'react-icons/fa6'
 
-const Searchbar = () => {
+const Searchbar = ({
+    filter, setFilter
+}) => {
     return (
         <div className='flexBetween pl-6 h-[3.3rem] bg-white w-full max-w-[366px] rounded-full ring-1 ring-slate-500/5'>
-            <input type="text" placeholder='Enter residency name/title/city/country' className='bg-transparent border-none outline-none w-full' />
+            <input
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+                type="text" placeholder='Enter residency name/title/city/country' className='bg-transparent border-none outline-none w-full' />
             <FaLocationDot className='relative right-4 text-xl hover:text-secondary cursor-pointer' />
         </div>
-    )
-}
+    );
+};
 
 export default Searchbar
--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Searchbar from '../components/Searchbar';
 import Item from '../components/Item';
 import useProperties from '../hooks/useProperties.jsx';
@@ -8,6 +8,7 @@ import { PuffLoader } from "react-spinners"
 const Listing = () => {
 
   const { data, isError, isLoading } = useProperties();
+  const [filter, setFilter] = useState("")
   // console.log(data);
   if (isError) {
     return (
@@ -27,13 +28,12 @@ const Listing = () => {
     <main className='max-padd-container my-[99px]'>
       <div className='max-padd-container py-10 xl:py-22 bg-primary rounded-3xl'>
         <div>
-          <Searchbar />
+          <Searchbar filter={filter} setFilter={setFilter} />
           {/* container */}
           <div className='grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10'>
-            {
-              data.map((property) => (
-                <Item key={property.title} property={property} />
-              ))
+            {data.filter((property) => property.title.toLowerCase().includes(filter.toLowerCase())).map((property) => (
+              <Item key={property.title} property={property} />
+            ))
             }
           </div>
         </div>
